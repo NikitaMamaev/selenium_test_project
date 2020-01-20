9c134d10a8f4b88c98567d4dca73de359d23bfb2
N.Mamaev <n.mamaev@ispsystem.com>
2020-01-20 19:02:31 +0800

    Переименование переменных со ссылками

--- test_main_page.py
+++ test_main_page.py
@@ -2,24 +2,23 @@ from .pages.main_page import MainPage
 from .pages.login_page import LoginPage
 
 
-
-link1 = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer"
-link2 = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
+main_url = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer"
+login_url = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
 
 
 def test_guest_can_go_to_login_page(browser):
-    page = MainPage(browser, link1)
+    page = MainPage(browser, main_url)
     page.open()
     page.go_to_login_page()
 
 
 def test_guest_should_see_login_link(browser):
-    page = MainPage(browser, link1)
+    page = MainPage(browser, main_url)
     page.open()
     page.should_be_login_link()
 
 
 def test_guest_is_in_login_page(browser):
-    page = LoginPage(browser, link2)
+    page = LoginPage(browser, login_url)
     page.open()
     page.should_be_login_page()
