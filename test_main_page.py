b426edc8101dcfe154d015bf147b1fc20cc00f2f
N.Mamaev <n.mamaev@ispsystem.com>
2020-01-29 10:51:46 +0800

    Добавление в тест перехода на страницу логина

--- test_main_page.py
+++ test_main_page.py
@@ -2,7 +2,7 @@ from .pages.main_page import MainPage
 from .pages.login_page import LoginPage
 
 
-main_url = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer"
+main_url = "http://selenium1py.pythonanywhere.com"
 login_url = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
 
 
@@ -10,6 +10,8 @@ def test_guest_can_go_to_login_page(browser):
     page = MainPage(browser, main_url)
     page.open()
     page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
 
 
 def test_guest_should_see_login_link(browser):
