e42c6b2aca2f20dc1ec367edd04823f9784b1ebf
N.Mamaev <n.mamaev@ispsystem.com>
2020-01-29 10:33:49 +0800

    Вынос комментариев из функции

--- pages/locators.py
+++ pages/locators.py
@@ -3,16 +3,16 @@ from selenium.webdriver.common.by import By
 
 
 class LoginPageLocators(BasePage):
+    # проверка на корректный url адрес
     def should_be_login_url(self):
-        # реализуйте проверку на корректный url адрес
         assert "login" in self.browser.current_url, 'Substring "login" is not in current url'
 
+    # проверка, что есть форма логина
     def should_be_login_form(self):
-        # реализуйте проверку, что есть форма логина
         assert self.is_element_present(
             By.CSS_SELECTOR, "#login_form"), "Login form is not presented"
 
+    # проверка, что есть форма регистрации на странице
     def should_be_register_form(self):
-        # реализуйте проверку, что есть форма регистрации на странице
         assert self.is_element_present(
             By.CSS_SELECTOR, "#register_form"), "Register form is not presented"
